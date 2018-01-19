33f7cb984bb0288468875c222f51294083bc5f1a
Servan42 <charlot.servan@gmail.com>
2018-01-19 12:38:48 +0100

    BabyStep non threadé

--- src/BabyStep/Client.java
+++ src/BabyStep/Client.java
@@ -1,5 +1,101 @@
 package BabyStep;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
 
 public class Client {
-
+	
+	int serverPort;
+	String serverHost;
+	Socket server;
+	
+	Client(){
+		serverPort = 1234;
+		serverHost = "127.0.0.1";
+	}
+	
+	public void lancer(){
+		try {
+			// Client connects to the server
+			server = new Socket(serverHost,serverPort);
+			System.out.println("Connected to " + server.getInetAddress());
+			
+			// Get the client's input stream
+			InputStream is = server.getInputStream();
+			DataInputStream dis = new DataInputStream(is);
+			int length = dis.readInt();
+			
+			// Read the data from input stream
+			byte[] b = new byte[length];
+			int num = 0;
+			while(num < length){
+				num = dis.read(b,0,length);
+			}
+			
+			//Transform data from bytes to String
+			String date = new String(b);
+			System.out.println("Server said: " + date);
+			
+			// Client sends bytes to server
+			
+			
+			is.close();
+			dis.close();
+			
+			is = server.getInputStream();
+			ObjectInputStream ois = new ObjectInputStream(is);
+			Date dateObject = (Date) ois.readObject();
+			System.out.println("Server said (object) : " + dateObject);
+			ois.close();
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void lancerObject() {
+		try {
+			// Client connects to the server
+			server = new Socket(serverHost,serverPort);
+			System.out.println("Connected to " + server.getInetAddress());
+			
+			// Get the client's input stream
+			InputStream is = server.getInputStream();
+			ObjectInputStream ois = new ObjectInputStream(is);
+			
+			// Read the data from input stream
+			Date dateObject = (Date) ois.readObject();
+			
+			//Transform data from bytes to String
+			System.out.println("Server said (object) : " + dateObject);
+			
+			// Client sends bytes to server
+			
+	
+			is.close();
+			ois.close();
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args){
+		Client client = new Client();
+		// Uncomment one of the following lines at a time.
+		//client.lancer();
+		client.lancerObject();
+	}
 }
--- src/BabyStep/Server.java
+++ src/BabyStep/Server.java
@@ -1,5 +1,108 @@
 package BabyStep;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
 public class Server {
 
+	int port;
+	ServerSocket server;
+	ServerSocket client;
+	
+	Server(){
+		port = 1234;
+		try {
+			// Create a server socket associated with port 1234
+			server = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void lancer() {
+		System.out.println("Server starded");
+		while(true){
+			Socket client;
+			try {
+				// Server waits for a connection
+				client = server.accept();
+				
+				// A client connected
+				System.out.println("Client " + client.getInetAddress() + " connected");
+				
+				//Server receives bytes from client
+				//Get the server's output stream
+				OutputStream os = client.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(os);
+				
+				//Build data and transform it to bytes
+				Date date = new Date();
+				
+				byte[] b = date.toString().getBytes();
+				dos.writeInt(b.length); // We put le number of bytes sent in the head of the message. 
+				
+				//Send the data to the client (Write in output stream)
+				dos.write(b);
+				
+				
+				// Closing the streams
+				os.close();
+				dos.close();
+				 
+				//Creer une methode lancer objet et recommencer avec les trucs ci dessous
+				os = client.getOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(os);
+				oos.writeObject(date);
+				oos.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void lancerObject() {
+		System.out.println("Server starded (object)");
+		while(true){
+			Socket client;
+			try {
+				// Server waits for a connection
+				client = server.accept();
+				
+				// A client connected
+				System.out.println("Client " + client.getInetAddress() + " connected");
+				
+				//Server receives bytes from client
+				//Get the server's output stream
+				OutputStream os = client.getOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(os);
+				os = client.getOutputStream();
+				//Build data
+				Date date = new Date();
+				
+				//Send the data to the client (Write in output stream)
+				oos.writeObject(date);
+				
+				// Closing the streams
+				os.close();
+				oos.close();
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		Server server = new Server();
+		// Uncomment one of the following lines at a time.
+		//server.lancer();
+		server.lancerObject();
+	}
+	
+	
 }
